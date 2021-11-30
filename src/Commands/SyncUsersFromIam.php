2e8f41c51c1f4408583822055334fb59bd83dd13
zed.zhao <954489346zyz@sina.com>
2021-11-30 18:55:12 +0800

    feat: 支持iam用户全量接口,批量用户同步脚本

--- src/Commands/SyncUsersFromIam.php
+++ src/Commands/SyncUsersFromIam.php
@@ -4,6 +4,7 @@
 namespace Ze\IAMAuth\Commands;
 
 use Illuminate\Console\Command;
+use Ze\IAMAuth\Models\AdminUserThirdPfBind;
 
 class SyncUsersFromIam extends Command
 {
@@ -13,7 +14,53 @@ class SyncUsersFromIam extends Command
 
     public function handle()
     {
-        // todo 先获取所有三方平台的用户列表
+        // IAM全量用户
+        $allGoldenUsers = collect(\IAMPassport::allUsers());
+
+        $goldenUsers = [];
+
+        foreach ($allGoldenUsers as $goldenUser) {
+            $goldenUsers[strtolower($goldenUser['username'])] = $goldenUser;
+        }
+
+        $userModel = config('admin.database.users_model');
+
+        // 分批次绑定账号操作
+        $userModel::chunk(100, function ($users) use ($goldenUsers) {
+
+            $users->each(function ($user) use ($goldenUsers) {
+                // 中台账号已绑定
+                if (AdminUserThirdPfBind::getBindRelationByUid(AdminUserThirdPfBind::IAM_PLATFORM, $user->id)) {
+                    return true;
+                }
+
+                $goldenUser = $goldenUsers[strtolower($user->username)] ?? [];
+
+                if (! $goldenUser) {
+                    $this->error('本地账号「' . $user->username . '」未找到匹配的高灯账号');
+                }
+
+                // iam账号已绑定
+                if (AdminUserThirdPfBind::getBindRelation(AdminUserThirdPfBind::IAM_PLATFORM, $goldenUser['userId'])) {
+                    $this->line('高灯账号「' . $goldenUser['username'] . '」已有绑定关系');
+                    return true;
+                }
+
+                // 未绑定过则增加绑定关系
+                AdminUserThirdPfBind::create([
+                    'user_id'       => $user->id,
+                    'platform'      => AdminUserThirdPfBind::IAM_PLATFORM,
+                    'third_user_id' => $goldenUser['userId'],
+                ]);
+
+                // 同步更新用户信息
+                $user->update(['name' => $goldenUser['fullname']]);
+
+                $this->info('本地账号「' . $user->username . '」绑定关系创建成功');
+            });
+
+        });
+
     }
 
 
